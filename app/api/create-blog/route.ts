96a149e74baaad3600f7a213e3da7565cadb65f7
Rami Abo Ajeeb <rami@zeour.co.uk>
2026-01-27 20:45:56 +0300

    create blog using server action | 27/01/2026

---
+++ app/api/create-blog/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server"
+
+export async function POST(){
+    console.log("hello from the server")
+
+    return NextResponse.json({
+        success:true
+    });
+}
